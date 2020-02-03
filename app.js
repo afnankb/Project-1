447e006e9635335df5e292ad5a552a5fd9bb2471
afnankb <afnankbasudan@gmail.com>
2020-02-03 11:37:22 +0300

    add img start game , add class to the img , remove pop message , tell the player x or o is win

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const eventType = 'click';
 let numberOfclick = 0;
 // create list to add if it's X or o 
 const XOrOList = [];
+let conterList = 0;
+//create list to add element un orderd
+const unorderList = [];
 // create query selector for all element 
 const number0 = document.querySelector('#number0');
 const number1 = document.querySelector('#number1');
@@ -14,9 +17,10 @@ const number5 = document.querySelector('#number5');
 const number6 = document.querySelector('#number6');
 const number7 = document.querySelector('#number7');
 const number8 = document.querySelector('#number8');
+const startgame = document.querySelector("#start");
 
 // funcition to play the sound when user click 
-function myPlay(){
+function myPlay() {
     var audio = new Audio("Click.mp3");
     audio.play();
 }
@@ -37,6 +41,7 @@ const myCallback0 = function () {
     numberOfclick++;
     img.setAttribute("src", scr);
     img.setAttribute("width", "200");
+    img.setAttribute("class", "imgGame");
     const newImg = document.querySelector("#number0");
     newImg.append(img);
     number0.removeEventListener(eventType, myCallback0)
@@ -51,6 +56,7 @@ const myCallback1 = function () {
     numberOfclick++;
     img.setAttribute("src", scr);
     img.setAttribute("width", "200");
+    img.setAttribute("class", "imgGame");
     const newImg = document.querySelector("#number1");
     newImg.append(img);
     newImg.removeEventListener(eventType, myCallback1)
@@ -66,6 +72,7 @@ const myCallback2 = function () {
     numberOfclick++;
     img.setAttribute("src", scr);
     img.setAttribute("width", "200");
+    img.setAttribute("class", "imgGame");
     const newImg = document.querySelector("#number2");
     newImg.append(img);
     number2.removeEventListener(eventType, myCallback2)
@@ -81,6 +88,7 @@ const myCallback3 = function () {
     numberOfclick++;
     img.setAttribute("src", scr);
     img.setAttribute("width", "200");
+    img.setAttribute("class", "imgGame");
     const newImg = document.querySelector("#number3");
     newImg.append(img);
     number3.removeEventListener(eventType, myCallback3)
@@ -95,6 +103,7 @@ const myCallback4 = function () {
     numberOfclick++;
     img.setAttribute("src", scr);
     img.setAttribute("width", "200");
+    img.setAttribute("class", "imgGame");
     const newImg = document.querySelector("#number4");
     newImg.append(img);
     number4.removeEventListener(eventType, myCallback4)
@@ -109,6 +118,7 @@ const myCallback5 = function () {
     numberOfclick++;
     img.setAttribute("src", scr);
     img.setAttribute("width", "200");
+    img.setAttribute("class", "imgGame");
     const newImg = document.querySelector("#number5");
     newImg.append(img);
     number5.removeEventListener(eventType, myCallback5)
@@ -122,6 +132,7 @@ const myCallback6 = function () {
     const scr = selectXorO(6);
     numberOfclick++;
     img.setAttribute("src", scr);
+    img.setAttribute("class", "imgGame");
     img.setAttribute("width", "200");
     const newImg = document.querySelector("#number6");
     newImg.append(img);
@@ -136,6 +147,7 @@ const myCallback7 = function () {
     const scr = selectXorO(7);
     numberOfclick++;
     img.setAttribute("src", scr);
+    img.setAttribute("class", "imgGame");
     img.setAttribute("width", "200");
     const newImg = document.querySelector("#number7");
     newImg.append(img);
@@ -150,6 +162,7 @@ const myCallback8 = function () {
     const scr = selectXorO(8);
     numberOfclick++;
     img.setAttribute("src", scr);
+    img.setAttribute("class", "imgGame");
     img.setAttribute("width", "200");
     const newImg = document.querySelector("#number8");
     newImg.append(img);
@@ -169,56 +182,65 @@ const winORLose = function () {
 
 
     if ((XOrOList[0] != null) && (XOrOList[1] != null) && (XOrOList[2] != null) && (XOrOList[0] === XOrOList[1]) && (XOrOList[0] === XOrOList[2])) {
-        number0.setAttribute("class","shake")
-        number1.setAttribute("class","shake")
-        number2.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
+        number0.setAttribute("class", "shake")
+        number1.setAttribute("class", "shake")
+        number2.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
 
     } else if ((XOrOList[3] != null) && (XOrOList[4] != null) && (XOrOList[5] != null) && (XOrOList[3] === XOrOList[4]) && (XOrOList[3] === XOrOList[5])) {
-        number3.setAttribute("class","shake")
-        number4.setAttribute("class","shake")
-        number5.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
+        number3.setAttribute("class", "shake")
+        number4.setAttribute("class", "shake")
+        number5.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
 
     } else if ((XOrOList[6] != null) && (XOrOList[7] != null) && (XOrOList[8] != null) && (XOrOList[6] === XOrOList[7]) && (XOrOList[6] === XOrOList[8])) {
-        number6.setAttribute("class","shake")
-        number7.setAttribute("class","shake")
-        number8.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
+        number6.setAttribute("class", "shake")
+        number7.setAttribute("class", "shake")
+        number8.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
 
     } else if ((XOrOList[0] != null) && (XOrOList[3] != null) && (XOrOList[6] != null) && (XOrOList[0] === XOrOList[3]) && (XOrOList[0] === XOrOList[6])) {
-        number0.setAttribute("class","shake")
-        number3.setAttribute("class","shake")
-        number6.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
+        number0.setAttribute("class", "shake")
+        number3.setAttribute("class", "shake")
+        number6.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
 
     } else if ((XOrOList[1] != null) && (XOrOList[4] != null) && (XOrOList[7] != null) && (XOrOList[1] === XOrOList[4]) && (XOrOList[1] === XOrOList[7])) {
-        number1.setAttribute("class","shake")
-        number4.setAttribute("class","shake")
-        number7.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
+        number1.setAttribute("class", "shake")
+        number4.setAttribute("class", "shake")
+        number7.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
 
     } else if ((XOrOList[2] != null) && (XOrOList[5] != null) && (XOrOList[8] != null) && (XOrOList[2] === XOrOList[5]) && (XOrOList[2] === XOrOList[8])) {
-        number2.setAttribute("class","shake")
-        number5.setAttribute("class","shake")
-        number8.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
+        number2.setAttribute("class", "shake")
+        number5.setAttribute("class", "shake")
+        number8.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
 
     } else if ((XOrOList[0] != null) && (XOrOList[4] != null) && (XOrOList[8] != null) && (XOrOList[0] === XOrOList[4]) && (XOrOList[0] === XOrOList[8])) {
-        number0.setAttribute("class","shake")
-        number4.setAttribute("class","shake")
-        number8.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
-           
+        number0.setAttribute("class", "shake")
+        number4.setAttribute("class", "shake")
+        number8.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
+
     } else if ((XOrOList[2] != null) && (XOrOList[4] != null) && (XOrOList[6] != null) && (XOrOList[2] === XOrOList[4]) && (XOrOList[2] === XOrOList[6])) {
-        number2.setAttribute("class","shake")
-        number4.setAttribute("class","shake")
-        number6.setAttribute("class","shake")
-        setTimeout("popMessageWin()", 500);
-     // if the number of click equal to 9 so no one win 
+        number2.setAttribute("class", "shake")
+        number4.setAttribute("class", "shake")
+        number6.setAttribute("class", "shake")
+        setTimeout("MessageWin()", 500);
+        setTimeout("playAgain()",600);
+        // if the number of click equal to 9 so no one win 
     } else if (numberOfclick === 9) {
-        setTimeout("popMessageTie()", 500);
-    
+        setTimeout("MessageTie()", 500);
+        setTimeout("playAgain()",600);
+
         // to know who is next 
 
     } else {
@@ -236,27 +258,31 @@ const winORLose = function () {
 
 }
 
-// pop massege to tell the user you are win 
-const popMessageWin = function () {
+//  massege to tell the user you are win 
+const MessageWin = function () {
 
-    var ask = window.confirm(" You  Are  win ");
-    if (ask) {
+    const lastElement = unorderList.length;
 
-        document.location.reload();
+    theWin = document.querySelector("#whoIsNext");
+    theWin.innerHTML = " the winner is " + unorderList[lastElement - 1];
 
-    }
 
+
+   
+    
 }
 
-// pop massege to tell the users you are tie 
-const popMessageTie = function () {
 
-    var ask = window.confirm(" Tie , No one win ");
-    if (ask) {
 
-        document.location.reload();
+//  massege to tell the users you are tie 
+const MessageTie = function () {
 
-    }
+    const lastElement = unorderList.length;
+
+    theWin = document.querySelector("#whoIsNext");
+    theWin.innerHTML = "   tie   ";
+
+
 
 }
 
@@ -265,16 +291,18 @@ const popMessageTie = function () {
 // select who is play 
 const selectXorO = function (index) {
     // x
-
+    const lastElement = unorderList.length;
     if (numberOfclick % 2 == 0) {
         // add it to the list 
         XOrOList[index] = "x";
+        unorderList[conterList++] = "x";
         return ("x.png")
 
     }
     // O
     else {
         XOrOList[index] = "o";
+        unorderList[conterList++] = "o";
         return ("o.png")
 
 
@@ -282,13 +310,34 @@ const selectXorO = function (index) {
 
 }
 
-// add event listener 
-number0.addEventListener(eventType, myCallback0);
-number1.addEventListener(eventType, myCallback1);
-number2.addEventListener(eventType, myCallback2);
-number3.addEventListener(eventType, myCallback3);
-number4.addEventListener(eventType, myCallback4);
-number5.addEventListener(eventType, myCallback5);
-number6.addEventListener(eventType, myCallback6);
-number7.addEventListener(eventType, myCallback7);
-number8.addEventListener(eventType, myCallback8);
+
+// const playAgain = function () {
+
+//     play();
+    
+//     const XOrOList = [];
+//     const unorderList = [];
+//     conterList = 0;
+//     numberOfclick=0;
+
+// }
+
+
+
+
+const play = function () {
+
+    number0.addEventListener(eventType, myCallback0);
+    number1.addEventListener(eventType, myCallback1);
+    number2.addEventListener(eventType, myCallback2);
+    number3.addEventListener(eventType, myCallback3);
+    number4.addEventListener(eventType, myCallback4);
+    number5.addEventListener(eventType, myCallback5);
+    number6.addEventListener(eventType, myCallback6);
+    number7.addEventListener(eventType, myCallback7);
+    number8.addEventListener(eventType, myCallback8);
+    console.log("work")
+
+}
+
+startgame.addEventListener(eventType, play);
